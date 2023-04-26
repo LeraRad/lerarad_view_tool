089856a468c0c8ad9652441ee9cf7065d4412a06
LeraRad <exlab7@gmail.com>
2023-04-26 22:26:47 +0200

    Lerarad Tool initial commit

---
+++ lib/lerarad_view_tool.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "lerarad_view_tool/version"
+require_relative "lerarad_view_tool/renderer"
+
+module LeraradViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
