9b78a9b5c73ef041ce52bc5dacf1c92a9c3d1ab0
LeraRad <exlab7@gmail.com>
2023-04-27 15:25:41 +0200

    added the code to the renderer.rb file

--- lib/lerarad_view_tool.rb
+++ lib/lerarad_view_tool.rb
@@ -5,11 +5,5 @@ require_relative "lerarad_view_tool/renderer"
 
 module LeraradViewTool
   class Error < StandardError; end
-  module LeraradViewTool
-  class Renderer
-    def self.copyright name, msg
-      "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
-    end
-  end
-end
+
 end
